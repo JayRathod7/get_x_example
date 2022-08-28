7588cb87450bae33a1830d09d52380e337a023a3
jayu <jayr77010@gmail.com>
2022-08-28 20:46:44 +0530

    minor changes

--- lib/controller/data_controller.dart
+++ lib/controller/data_controller.dart
@@ -3,9 +3,8 @@ import 'package:get_x/api_service/api_service.dart';
 import 'package:get_x/model/post_model.dart';
 
 class DataController extends GetxController {
-  Rx<PostModel> data = PostModel().obs;
   RxList<PostModel> dataList = <PostModel>[].obs;
-
+  var isLoding = false.obs;
   @override
   void onInit() {
     super.onInit();
@@ -13,8 +12,10 @@ class DataController extends GetxController {
   }
 
   void fetchData() async {
+    isLoding.value = true;
     List<PostModel> postModel = await ApiService.getData();
     if (postModel != null) {
+      isLoding.value = false;
       dataList.value = postModel as List<PostModel>;
     } else {
       print("something went wrong");
--- lib/screen/first_screen.dart
+++ lib/screen/first_screen.dart
@@ -19,44 +19,40 @@ class _FirstScreenState extends State<FirstScreen> {
         appBar: AppBar(
           title: Text("getX Example"),
         ),
-        body: Obx(() => ListView.separated(
-              separatorBuilder: (context, index) {
-                return Divider();
-              },
-              itemCount: dataController.dataList.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  decoration: BoxDecoration(border: Border.all(),borderRadius: BorderRadius.circular(12)),
-                    padding: EdgeInsets.only(top: 10, bottom: 10,left: 10,right: 10),
-                    margin: EdgeInsets.only(top: 10, bottom: 10,left: 10,right: 10),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Box(
-                            name: dataController.dataList.value[index].id
-                                .toString()),
-                        SizedBox(height: 6),
-                        Box(
-                            name: dataController.dataList.value[index].body
-                                .toString()),                        SizedBox(height: 6),
-
-                        Box(
-                            name: dataController.dataList.value[index].userId
-                                .toString()),                        SizedBox(height: 6),
-
-                      ],
-                    ));
-              },
-            )));
+        body: Obx(() => dataController.isLoding.value
+            ? Container(
+                child: Center(child: CircularProgressIndicator()),
+              )
+            : ListView.separated(
+                separatorBuilder: (context, index) {
+                  return Divider();
+                },
+                itemCount: dataController.dataList.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(),
+                          borderRadius: BorderRadius.circular(12)),
+                      padding: EdgeInsets.only(
+                          top: 10, bottom: 10, left: 10, right: 10),
+                      margin: EdgeInsets.only(
+                          top: 10, bottom: 10, left: 10, right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(dataController.dataList.value[index].id
+                              .toString()),
+                          SizedBox(height: 6),
+                          Text(dataController.dataList.value[index].userId
+                              .toString()),
+                          SizedBox(height: 6),
+                          Text(dataController.dataList.value[index].title
+                              .toString()),
+                          SizedBox(height: 6),
+                        ],
+                      ));
+                },
+              )));
   }
-
-  Widget Box({required String name}) => Container(
-      padding: EdgeInsets.only(left: 10, right: 10, top: 10, bottom: 10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(12), border: Border.all()),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(name,maxLines: 10,),
-      ));
 }
